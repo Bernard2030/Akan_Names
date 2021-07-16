6e95c80e08debe242ae0d6cfb6fd05e58b8ee38a
Bernard2030 <bernard.mukhenye@student.moringa.com>
2021-07-16 23:36:56 +0300

    add initial js

---
+++ index.js
@@ -0,0 +1,40 @@
+// function akanNames() {
+//     // collect forms
+
+// let inputMonth = document.getElementById("month").value;
+// let inputDay = document.getElementsById("day");
+// let inputYear = document.getElementsById("year");
+// let gender = document.getElementsByName("gender");
+
+
+// try {
+
+//     if (inputDate == "") throw "Empty"
+// }
+
+// catch {
+//     document.getElementById("emptyDate").innerHTML = "*Date field required"
+    
+// }
+
+
+
+
+
+
+// var month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+// var gender = 'Male'
+// var gender = 'Female'
+// var dob = m + " " + d+ ", "+y;
+// var then = new Date(dob);
+// var theDay = then.getDay();
+// var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+// var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+// var weekDay =['Sunday','Monday','Tuesdays','Wednesday','Thursday','Friday','Saturday'];
+// if(g==="Male"){
+//     console.log("Your akan name is: "+akanMale[theDay] +"Born on :"+weekDay[theDay]);
+// }
+// if(g === 'Female'){
+//     console.log("Your akan name is: "+akanFemale[theDay] +" Born on :"+weekDay[theDay]);
+// }
+// }
