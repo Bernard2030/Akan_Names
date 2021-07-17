1e366c436d6b8958af250323ed43805196f25d75
Bernard2030 <bernard.mukhenye@student.moringa.com>
2021-07-17 16:46:16 +0300

    add js content

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-// function akanNames() {
-//     // collect forms
-
-// let inputMonth = document.getElementById("month").value;
-// let inputDay = document.getElementsById("day");
-// let inputYear = document.getElementsById("year");
-// let gender = document.getElementsByName("gender");
 
 
-// try {
+function akanNames() {
 
-//     if (inputDate == "") throw "Empty"
-// }
+    let inputMonth = document.getElementById("month").value;
+    let inputDay = document.getElementById("day").value;
+    let inputYear = document.getElementById("year").value;
+    let gender = document.getElementsByName("gender").value;
 
-// catch {
-//     document.getElementById("emptyDate").innerHTML = "*Date field required"
-    
-// }
+    if (day <= 0 || day > 31) {
+        alert("enter valid date")
+    } else if (inputYear.length <= 4) {
+        alert("Year input invalid")
+    } else if (inputMonth.length = ".") {
+        alert("Month input required")
+    } else {
+        alert("All fields MUST be field!!")
+    }
 
 
+    //  function centuryFromYear(year) {
+    //      return Math.ceil(year/100)
+    //  }
 
+    // var  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    // var d = new Date();
+    // var monthName=months[d.getMonth()];
 
+    var month = inputMonth
+    var day = inputDay
+    var year = inputYear
+    var gender = ''
+    var dob = month + " " + day + ", " + year;
+    var then = new Date(dob);
+    var theDay = then.getDay();
+    var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var weekDay = ['Sunday', 'Monday', 'Tuesdays', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    if (gender === "Male") {
+        alert("Your akan name is: " + akanMale[theDay] + " Born on :" + weekDay[theDay]);
+    }
+    if (gender === 'Female') {
+        alert("Your akan name is: " + akanFemale[theDay] + " Born on :" + weekDay[theDay]);
+    }
 
-
-// var month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-// var gender = 'Male'
-// var gender = 'Female'
-// var dob = m + " " + d+ ", "+y;
-// var then = new Date(dob);
-// var theDay = then.getDay();
-// var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-// var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-// var weekDay =['Sunday','Monday','Tuesdays','Wednesday','Thursday','Friday','Saturday'];
-// if(g==="Male"){
-//     console.log("Your akan name is: "+akanMale[theDay] +"Born on :"+weekDay[theDay]);
-// }
-// if(g === 'Female'){
-//     console.log("Your akan name is: "+akanFemale[theDay] +" Born on :"+weekDay[theDay]);
-// }
-// }
+}
